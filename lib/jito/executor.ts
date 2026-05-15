19222310574a07e1b5c98f89a1ecc624198b4c78
overandor <j.skrobynets@gmail.com>
2026-05-15 18:58:03 -0400

    Add Jito execution runtime

---
+++ lib/jito/executor.ts
@@ -0,0 +1,19 @@
+import { createSettlementBundle } from './bundles'
+import { createFinalityRecord, advanceFinality } from '@/lib/settlement/finality'
+
+export type JitoExecutionResult = {
+  bundleId: string
+  finality: ReturnType<typeof advanceFinality>
+  engine: 'jito'
+}
+
+export function executeJitoBundle(claimIds: string[]): JitoExecutionResult {
+  const bundle = createSettlementBundle(claimIds)
+  const finality = advanceFinality(createFinalityRecord(), 'submitted', 1)
+
+  return {
+    bundleId: bundle.bundleId,
+    finality,
+    engine: 'jito'
+  }
+}

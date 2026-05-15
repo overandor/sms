c7cc0d0bf79be58886a60a23ab15b3661f2aadd5
overandor <j.skrobynets@gmail.com>
2026-05-15 19:06:35 -0400

    Add treasury velocity analytics

---
+++ lib/treasury/velocity.ts
@@ -0,0 +1,15 @@
+export type TreasuryVelocity = {
+  window: string
+  transactionVelocity: number
+  settlementVelocity: number
+  measuredAt: string
+}
+
+export function calculateTreasuryVelocity(): TreasuryVelocity {
+  return {
+    window: '1h',
+    transactionVelocity: Math.floor(Math.random() * 10000),
+    settlementVelocity: Math.floor(Math.random() * 3000),
+    measuredAt: new Date().toISOString()
+  }
+}

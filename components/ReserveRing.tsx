08ae80a9447a659925a15d4ee84e33cc5a49b2ea
overandor <j.skrobynets@gmail.com>
2026-05-15 18:48:29 -0400

    Add ReserveRing component

---
+++ components/ReserveRing.tsx
@@ -0,0 +1,10 @@
+export function ReserveRing() {
+  return (
+    <div className='reserve-ring'>
+      <div className='reserve-ring__inner'>
+        <div className='reserve-ring__percent'>100%</div>
+        <div className='reserve-ring__label'>Reserve Backed</div>
+      </div>
+    </div>
+  )
+}
